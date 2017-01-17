4514467895610fceb13ce4fac09f7d9bb2430a0f
abattoiry <abattoiryue@163.com>
2017-01-17 16:26:28 +0900

    JS handler

--- src/com/kii/virtualdevice/JSHandler.java
+++ src/com/kii/virtualdevice/JSHandler.java
@@ -1,7 +1,25 @@
 package com.kii.virtualdevice;
 
+import org.json.JSONObject;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 /**
  * Created by yue on 17/01/2017.
  */
 public class JSHandler {
+
+    static JSONObject handle(String JSScript, JSONObject input) throws ScriptException, NoSuchMethodException {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("JavaScript");
+        engine.eval(JSScript);
+        Invocable inv = (Invocable) engine;
+        inv.invokeFunction("interpret", input);
+        Object output = engine.get("output");
+        return new JSONObject(output);
+    }
+    
 }
